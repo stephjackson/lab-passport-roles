e16d821fb0cc7c1da48d2e5ccfb57e93d589b3b1
stephjackson <steph.garrett.jackson@gmail.com>
2018-02-20 16:16:31 -0500

    Add iteration 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,6 +5,11 @@ const logger       = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
 const mongoose     = require("mongoose");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require('./models/user');
 
 const app = express();
 
@@ -25,6 +30,41 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Routes
 app.use("/", siteController);
--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -1,8 +1,98 @@
 const express = require("express");
 const siteController = express.Router();
+var checkBoss  = checkRoles('Boss');
+var checkDevelop = checkRoles('Developer');
+var checkTA = checkRoles('TA');
+const passport = require(`passport`);
+const User = require('../models/user');
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
 siteController.get("/", (req, res, next) => {
   res.render("index");
 });
+
+siteController.get(`/login`, (req, res, next) => {
+  res.render(`login`);
+})
+
+siteController.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+siteController.get(`/showusers`, checkBoss, (req, res, next) => {
+  User.find({}, (err, users)=> {
+    if (err) { return next(err) }
+
+    res.render(`showusers`, {
+      users: users
+    })
+  })
+})
+
+siteController.get("/addemployee", checkBoss, (req, res, next) => {
+  res.render("addemployee");
+});
+
+siteController.post(`/addemployee`, checkBoss, (req, res, next) => {
+  const username = req.body.username;
+  const name = req.body.name;
+  const familyName = req.body.familyName;
+  const password = req.body.password;
+  const role = req.body.role;
+
+  if (username === "" || password === "" || name === `` || familyName === ``) {
+    res.render("addemployee", { message: "Please enter all fields." });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render(`addemployee`, { message: `The username already exists!` });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      name,
+      familyName,
+      password: hashPass,
+      role
+    })
+
+    newUser.save((err) => {
+      if (err) {
+        res.render(`addemployee`, { message: `Something went wrong`} )
+      } else {
+        res.redirect("/")
+      }
+    })
+  })
+})
+
+siteController.post(`/:id/delete`, checkBoss, (req, res, next) => {
+  const id = req.params.id;
+
+  User.findByIdAndRemove(id, (err, user) => {
+    if (err) { return next(err) ;}
+    return res.redirect(`/showusers`);
+  })
+})
+
+function checkRoles(role) {
+  return function(req, res, next) {
+    if (req.isAuthenticated() && req.user.role === role) {
+      return next();
+    } else {
+      res.redirect('/login')
+    }
+  }
+}
 
 module.exports = siteController;
